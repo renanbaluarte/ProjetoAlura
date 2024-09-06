ed840c89487b64bf8e461f1d77a809b01be72304
Renan <rdias285@hotmail.com>
2024-09-05 21:54:24 -0300

    novos dubladores

---
+++ dados.js
@@ -0,0 +1,116 @@
+
+let dubladores = [
+
+    {
+        nome: "Guilherme Briggs",
+        descricao: "Guilherme Briggs é um renomado dublador brasileiro conhecido por sua versatilidade e talento na dublagem de diversos personagens icônicos. Com uma carreira que abrange desde animações até videogames, Briggs é famoso por dar voz a personagens como o Megatron em 'Transformers' e o Kero-chan em 'Sakura Card Captors'. Sua habilidade em criar vozes distintas e expressivas o tornou uma figura destacada na indústria de dublagem no Brasil.",
+        personagem: "Megatron em Transformers, Kero-chan em Sakura Card Captors, Superman em Liga da Justiça, Buzz Lightyear em Toy Story, e Hamtaro em Hamtaro.",
+        link: "https://pt.wikipedia.org/wiki/Guilherme_Briggs"
+    },
+
+    {
+        nome: "Wendel Bezerra",
+        descricao: "Wendel Bezerra é um renomado dublador brasileiro, famoso por dar voz a personagens icônicos como o Goku em 'Dragon Ball' e o Bob Esponja em 'Bob Esponja Calça Quadrada'. Sua carreira é marcada por uma ampla gama de papéis em animações e videogames, consolidando-se como uma das vozes mais reconhecíveis do Brasil.",
+        personagem: "Goku em Dragon Ball, Bob Esponja em Bob Esponja Calça Quadrada, Samwise Gamgi em O Senhor dos Anéis, Jackie Chan em As Aventuras de Jackie Chan, e Donatello em As Tartarugas Ninja.",
+        link: "https://pt.wikipedia.org/wiki/Wendel_Bezerra"
+    },
+
+    {
+        nome: "Manolo Rey",
+        descricao: "Manolo Rey é um renomado dublador brasileiro, conhecido por dar vida a diversos personagens icônicos em animações e filmes. Sua voz versátil e talento em criar personalidades distintas o tornaram um dos dubladores mais respeitados do Brasil.",
+        personagem: "Tobey Maguire na trilogia do Homem-Aranha, Will Smith em Um Maluco no Pedaço, Robin em Os Jovens Titãs, Sonic em Sonic the Hedgehog, e Gaguinho em Looney Tunes.",
+        link: "https://dublagem.fandom.com/wiki/Manolo_Rey"
+    },
+
+    {
+        nome: "Mauro Ramos",
+        descricao: "Mauro Ramos é um dublador brasileiro conhecido por sua voz grave e marcante. Ele é famoso por dublar personagens como o Pumba em 'O Rei Leão' e o Sulley em 'Monstros S.A.'.",
+        personagem: "Pumba em O Rei Leão, Sulley em Monstros S.A., Mumm-Ra em Thundercats, e Rei Tritão em A Pequena Sereia.",
+        link: "https://dublagem.fandom.com/wiki/Mauro_Ramos"
+    },
+
+    {
+        nome: "Selma Lopes",
+        descricao: "Selma Lopes é uma dubladora brasileira icônica, conhecida por dar voz à Marge Simpson em 'Os Simpsons'. Sua carreira abrange várias décadas e inclui uma vasta gama de personagens.",
+        personagem: "Marge Simpson em Os Simpsons, Vovó Piedade em A Família Addams, e Ursula em A Pequena Sereia.",
+        link: "https://dublagem.fandom.com/wiki/Selma_Lopes"
+    },
+
+    {
+        nome: "Orlando Drummond",
+        descricao: "Orlando Drummond foi um lendário dublador brasileiro, famoso por dar voz ao Scooby-Doo e ao Vingador em 'Caverna do Dragão'. Sua carreira durou mais de 70 anos.",
+        personagem: "Scooby-Doo em Scooby-Doo, Vingador em Caverna do Dragão, Alf em Alf, o ETeimoso, e Popeye em Popeye.",
+        link: "https://dublagem.fandom.com/wiki/Orlando_Drummond"
+    },
+
+    {
+        nome: "Mônica Rossi",
+        descricao: "Mônica Rossi é uma dubladora brasileira conhecida por sua voz suave e expressiva. Ela é famosa por dublar a Princesa Leia em 'Star Wars' e a Jean Grey em 'X-Men'.",
+        personagem: "Princesa Leia em Star Wars, Jean Grey em X-Men, e Lisa Simpson em Os Simpsons.",
+        link: "https://dublagem.fandom.com/wiki/Mônica_Rossi"
+    },
+
+    {
+        nome: "Rodrigo Lombardi",
+        descricao: "Rodrigo Lombardi é um ator e dublador brasileiro, conhecido por dublar o personagem Nick Wilde em 'Zootopia'.",
+        personagem: "Nick Wilde em Zootopia, e Flynn Rider em Enrolados.",
+        link: "https://dublagem.fandom.com/wiki/Rodrigo_Lombardi"
+    },
+
+    {
+        nome: "Fernanda Bullara",
+        descricao: "Fernanda Bullara é uma dubladora brasileira conhecida por sua voz jovem e enérgica. Ela é famosa por dublar a Sakura em 'Naruto' e a Hermione Granger em 'Harry Potter'.",
+        personagem: "Sakura em Naruto, Hermione Granger em Harry Potter, e Mabel Pines em Gravity Falls.",
+        link: "https://dublagem.fandom.com/wiki/Fernanda_Bullara"
+    },
+
+    {
+        nome: "Cecília Lemes",
+        descricao: "Cecília Lemes é uma dubladora brasileira conhecida por sua voz doce e carismática. Ela é famosa por dublar a Chiquinha em 'Chaves' e a Sailor Moon em 'Sailor Moon'.",
+        personagem: "Chiquinha em Chaves, Sailor Moon em Sailor Moon, e Angelica em Rugrats: Os Anjinhos.",
+        link: "https://dublagem.fandom.com/wiki/Cecília_Lemes"
+    },
+
+    {
+        nome: "Isaac Bardavid",
+        descricao: "Isaac Bardavid foi um dublador brasileiro icônico, conhecido por sua voz grave e poderosa. Ele é famoso por dublar o Wolverine em 'X-Men' e o Esqueleto em 'He-Man'.",
+        personagem: "Wolverine em X-Men, Esqueleto em He-Man, e Tigrão em Ursinho Pooh.",
+        link: "https://dublagem.fandom.com/wiki/Isaac_Bardavid"
+    },
+
+    {
+        nome: "Márcio Simões",
+        descricao: "Márcio Simões é um dublador brasileiro conhecido por sua versatilidade e talento. Ele é famoso por dublar o Gênio em 'Aladdin' e o Coringa em várias animações da DC.",
+        personagem: "Gênio em Aladdin, Coringa em Batman: A Série Animada, e Hades em Hércules.",
+        link: "https://dublagem.fandom.com/wiki/Márcio_Simões"
+    },
+
+    {
+        nome: "Adriana Torres",
+        descricao: "Adriana Torres é uma dubladora brasileira conhecida por sua voz doce e expressiva. Ela é famosa por dublar a Princesa Jujuba em 'Hora de Aventura' e a Mabel Pines em 'Gravity Falls'.",
+        personagem: "Princesa Jujuba em Hora de Aventura, Mabel Pines em Gravity Falls, e Vanellope em Detona Ralph.",
+        link: "https://dublagem.fandom.com/wiki/Adriana_Torres"
+    },
+
+    {
+        nome: "Ricardo Schnetzer",
+        descricao: "Ricardo Schnetzer é um dublador brasileiro conhecido por sua voz marcante e versátil. Ele é famoso por dublar o James Bond em vários filmes da franquia e o Capitão Kirk em 'Star Trek'.",
+        personagem: "James Bond em 007, Capitão Kirk em Star Trek, e Indiana Jones em Indiana Jones.",
+        link: "https://dublagem.fandom.com/wiki/Ricardo_Schnetzer"
+    },
+
+    {
+        nome: "Letícia Quinto",
+        descricao: "Letícia Quinto é uma dubladora brasileira conhecida por sua voz suave e expressiva. Ela é famosa por dublar a Saori Kido em 'Os Cavaleiros do Zodíaco' e a Bulma em 'Dragon Ball'.",
+        personagem: "Saori Kido em Os Cavaleiros do Zodíaco, Bulma em Dragon Ball, e Kagome em Inuyasha.",
+        link: "https://dublagem.fandom.com/wiki/Letícia_Quinto"
+    },
+
+    {
+        nome: "Fábio Lucindo",
+        descricao: "Fábio Lucindo é um dublador brasileiro conhecido por sua voz jovem e enérgica. Ele é famoso por dublar o Ash Ketchum em 'Pokémon' e o Kuririn em 'Dragon Ball'.",
+        personagem: "Ash Ketchum em Pokémon, Kuririn em Dragon Ball, e Arnold em Hey Arnold!",
+        link: "https://dublagem.fandom.com/wiki/Fábio_Lucindo"
+    }
+
+];
